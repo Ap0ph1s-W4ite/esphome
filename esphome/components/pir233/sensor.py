fe71776ad4314cdae397f2c0f471fe1df2aacb4c
Adriano Brum <adriano.j.brum@criticalsoftware.com>
2025-06-19 20:19:31 +0100

    configuration and code part defined

--- esphome/components/pir233/sensor.py
+++ esphome/components/pir233/sensor.py
@@ -27,29 +27,17 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PIR233Component),
         cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_SENSITIVITY): number.number_schema(
+        cv.Optional(CONF_SENSITIVITY, default=3): number.number_schema(
             PIR233Gain,
             icon=ICON_SENSITIVITY,
             unit_of_measurement=UNIT_EMPTY,
             entity_category=ENTITY_CATEGORY_CONFIG,
-        ).extend(
-            {
-                cv.Optional(CONF_INITIAL_VALUE, default=5): cv.int_range(min=0, max=10),
-                cv.Optional(CONF_MIN_VALUE, default=0): cv.int_,
-                cv.Optional(CONF_MAX_VALUE, default=10): cv.int_,
-            }
         ),
-        cv.Optional(CONF_PERSISTENCE, default=0): number.number_schema(
+        cv.Optional(CONF_PERSISTENCE, default=1): number.number_schema(
             PIR233Persistence,
             icon=ICON_PERSISTENCE,
             unit_of_measurement=UNIT_EMPTY,
             entity_category=ENTITY_CATEGORY_CONFIG,
-        ).extend(
-            {
-                cv.Optional(CONF_INITIAL_VALUE, default=0): cv.int_range(min=0, max=10),
-                cv.Optional(CONF_MIN_VALUE, default=0): cv.int_,
-                cv.Optional(CONF_MAX_VALUE, default=10): cv.int_,
-            }
         ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -58,6 +46,15 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     pin = await gpio_pin_expression(config[CONF_PIN])
-    cg.add(var.set_output_pin(pin))
+    cg.add(var.set_pin(pin))
+    sensitivity = await number.new_number(
+        config[CONF_SENSITIVITY], min_value=1, max_value=10, step=1
+    )
+    await cg.register_parented(sensitivity, var)
+    cg.add(var.set_sensitivity(sensitivity))
+    persistence = await number.new_number(
+        config[CONF_PERSISTENCE], min_value=1, max_value=10, step=1
+    )
+    await cg.register_parented(persistence, var)
+    cg.add(var.set_persistence(persistence))
