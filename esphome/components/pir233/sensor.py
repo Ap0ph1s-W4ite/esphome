53297f90b0a5ebef1416f5fc9261f9d5c22babbf
Adriano Brum <adriano.j.brum@criticalsoftware.com>
2025-06-19 19:01:34 +0100

    configuration part

--- esphome/components/pir233/sensor.py
+++ esphome/components/pir233/sensor.py
@@ -1,16 +1,56 @@
 from esphome import pins
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PIN
+from esphome.components import number
+from esphome.const import (
+    ENTITY_CATEGORY_CONFIG,
+    UNIT_EMPTY,
+    CONF_PIN,
+    CONF_INITIAL_VALUE,
+    CONF_MIN_VALUE,
+    CONF_MAX_VALUE,
+    CONF_ID,
+    CONF_SENSITIVITY,
+)
 from esphome.cpp_helpers import gpio_pin_expression
 
 pir233_component_ns = cg.esphome_ns.namespace("pir233_component")
 PIR233Component = pir233_component_ns.class_("PIR233Component", cg.Component)
+PIR233Gain = pir233_component_ns.class_("PIR233Gain", cg.Component)
+PIR233Persistence = pir233_component_ns.class_("PIR233Persistence", cg.Component)
+
+CONF_PERSISTENCE = "persistence"
+ICON_SENSITIVITY = "mdi:tune"
+ICON_PERSISTENCE = "mdi:timer-sand"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PIR233Component),
         cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_SENSITIVITY): number.number_schema(
+            PIR233Gain,
+            icon=ICON_SENSITIVITY,
+            unit_of_measurement=UNIT_EMPTY,
+            entity_category=ENTITY_CATEGORY_CONFIG,
+        ).extend(
+            {
+                cv.Optional(CONF_INITIAL_VALUE, default=5): cv.int_range(min=0, max=10),
+                cv.Optional(CONF_MIN_VALUE, default=0): cv.int_,
+                cv.Optional(CONF_MAX_VALUE, default=10): cv.int_,
+            }
+        ),
+        cv.Optional(CONF_PERSISTENCE, default=0): number.number_schema(
+            PIR233Persistence,
+            icon=ICON_PERSISTENCE,
+            unit_of_measurement=UNIT_EMPTY,
+            entity_category=ENTITY_CATEGORY_CONFIG,
+        ).extend(
+            {
+                cv.Optional(CONF_INITIAL_VALUE, default=0): cv.int_range(min=0, max=10),
+                cv.Optional(CONF_MIN_VALUE, default=0): cv.int_,
+                cv.Optional(CONF_MAX_VALUE, default=10): cv.int_,
+            }
+        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
