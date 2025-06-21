f5b277c205f3fbbb07cee4a1773ca83428e1cd2e
Ap0ph1s-W4ite <>
2025-06-21 18:22:01 +0100

    Refactor PIR233 component schema to include detection interval and update pin schema

--- esphome/components/pir233/sensor.py
+++ esphome/components/pir233/sensor.py
@@ -13,25 +13,28 @@ from esphome.cpp_helpers import gpio_pin_expression
 
 pir233_component_ns = cg.esphome_ns.namespace("pir233_component")
 PIR233Component = pir233_component_ns.class_("PIR233Component", cg.Component)
-PIR233Gain = pir233_component_ns.class_("PIR233Gain", cg.Component)
-PIR233Persistence = pir233_component_ns.class_("PIR233Persistence", cg.Component)
 
 CONF_PERSISTENCE = "persistence"
+CONF_DETECTION_INTERVAL = "detection_interval"
+ICON_DETECTION_INTERVAL = "mdi:timer"
 ICON_SENSITIVITY = "mdi:tune"
 ICON_PERSISTENCE = "mdi:timer-sand"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PIR233Component),
-        cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_SENSITIVITY, default=3): number.number_schema(
-            PIR233Gain,
+        cv.Required(CONF_PIN): pins.gpio_pin_schema,
+        cv.Optional(CONF_DETECTION_INTERVAL, default=15): number.NUMBER_SCHEMA.extend(
+            icon=ICON_DETECTION_INTERVAL,
+            unit_of_measurement="s",
+            entity_category=ENTITY_CATEGORY_CONFIG,
+        ),
+        cv.Optional(CONF_SENSITIVITY, default=3): number.NUMBER_SCHEMA.extend(
             icon=ICON_SENSITIVITY,
             unit_of_measurement=UNIT_EMPTY,
             entity_category=ENTITY_CATEGORY_CONFIG,
         ),
-        cv.Optional(CONF_PERSISTENCE, default=1): number.number_schema(
-            PIR233Persistence,
+        cv.Optional(CONF_PERSISTENCE, default=1): number.NUMBER_SCHEMA.extend(
             icon=ICON_PERSISTENCE,
             unit_of_measurement=UNIT_EMPTY,
             entity_category=ENTITY_CATEGORY_CONFIG,
@@ -55,3 +58,8 @@ async def to_code(config):
     )
     await cg.register_parented(persistence, var)
     cg.add(var.set_persistence(persistence))
+    detection_interval = await number.new_number(
+        config[CONF_DETECTION_INTERVAL], min_value=1, max_value=60, step=1
+    )
+    await cg.register_parented(detection_interval, var)
+    cg.add(var.set_detection_interval(detection_interval))
