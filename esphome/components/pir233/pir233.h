e73f393cdec9498f8aaec811ec7af43a8ab653fe
Ap0ph1s-W4ite <adjbrum@gmail.com>
2025-06-25 22:49:01 +0100

    Refactor PIR233Component: correct inheritance from binary_sensor and update validate_data method signature

--- esphome/components/pir233/pir233.h
+++ esphome/components/pir233/pir233.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
+#include "esphome/core/gpio.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
@@ -10,7 +11,7 @@ namespace pir233 {
 static const uint32_t VALIDATION_MASK = 0x30001;
 static const uint32_t EXPECTED_VALID_MASK = 0x20000;
 
-class PIR233Component : public Component, public BinarySensor {
+class PIR233Component : public Component, public binary_sensor::BinarySensor {
  public:
   void set_pin(GPIOPin *pin) { pin_ = pin; }
   void set_detection_interval(uint8_t detection_interval) { detection_interval_ = detection_interval; }
@@ -28,7 +29,7 @@ class PIR233Component : public Component, public BinarySensor {
 
   uint8_t read_bit_pattern_();
   uint32_t motion_data_();
-  bool validate_data_();
+  bool validate_data_(uint32_t data);
 
   GPIOPin *pin_;
 };
